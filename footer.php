c554a7dc603a6f1f714e28b25ac8b9856002b5e0
Rk King <rkking@live.com>
2015-12-05 23:48:35 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="site-footer">
+<small> &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | Winter 15 Theme. Crafted with heart in India </small>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</div> <!-- container -->
+</html>
